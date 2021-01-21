656a9cd4364f25da6a737b9465278c41a01f7dfc
Alexander <nefedov.alexander91@yandex.ru>
2021-01-22 04:10:24 +0700

    JJ-1 Simplest possible calculator

---
+++ calc1.py
@@ -0,0 +1,88 @@
+# 2021.01.22
+
+INTEGER, PLUS, EOF = "INTEGER", "PLUS", "EOF"
+
+
+class Token:
+    def __init__(self, ttype, value):
+        self.ttype = ttype
+        self.value = value
+
+    def __str__(self):
+        return f"Token({self.ttype}, {self.value})"
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class Interpreter:
+    def __init__(self, text):
+        self.text = text
+        self.pos = 0
+        self.current_token = None
+
+    def error(self):
+        raise Exception("Error parsing input")
+
+    def get_next_token(self):
+        """
+        Lexical analyzer (also known as scanner or tokenizer)
+
+        This method is responsible for braking a sentence apart into tokens.
+        One token at a time.
+        """
+        text = self.text
+        if self.pos > len(text) - 1:
+            return Token(EOF, None)
+
+        current_char = text[self.pos]
+
+        if current_char.isdigit():
+            token = Token(INTEGER, int(current_char))
+            self.pos += 1
+            return token
+
+        if current_char == "+":
+            token = Token(PLUS, current_char)
+            self.pos += 1
+            return token
+
+        self.error()
+
+    def eat(self, token_type):
+        if self.current_token.ttype == token_type:
+            self.current_token = self.get_next_token()
+        else:
+            self.error()
+
+    def expr(self):
+        self.current_token = self.get_next_token()
+
+        left = self.current_token
+        self.eat(INTEGER)
+
+        op = self.current_token
+        self.eat(PLUS)
+
+        right = self.current_token
+        self.eat(INTEGER)
+
+        result = left.value + right.value
+        return result
+
+
+def main():
+    while True:
+        try:
+            text = input("calc> ")
+        except EOFError:
+            break
+        if not text:
+            continue
+        interpreter = Interpreter(text)
+        result = interpreter.expr()
+        print(result)
+
+
+if __name__ == "__main__":
+    main()
