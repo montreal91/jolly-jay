22b020a775bae3d98d15aa2baa7f3f9c3185c397
Alexander <nefedov.alexander91@yandex.ru>
2021-01-30 10:54:23 +0700

    JJK-6 Tests with randomized values

--- kotlin/src/test/kotlin/interpreter_test.kt
+++ kotlin/src/test/kotlin/interpreter_test.kt
@@ -1,10 +1,14 @@
 package interpreter
 
+
 import org.junit.Test
 import org.junit.Assert.assertEquals
 
 
 private val THE_ANSWER = 42
+
+private val MIN = 100
+private val MAX = 10000
 
 
 class InterpreterTest {
@@ -15,9 +19,24 @@ class InterpreterTest {
   }
 
   @Test
+  fun TestNumberRnd() {
+    val x = (MIN..MAX).random()
+    val i = MakeInterpreter("$x")
+    assertEquals(x, i.Execute())
+  }
+
+  @Test
   fun TestPlus() {
     val i = MakeInterpreter("11 + 31")
     assertEquals(THE_ANSWER, i.Execute())
+  }
+
+  @Test
+  fun TestPlusRnd() {
+    val x1 = (MIN..MAX).random()
+    val x2 = (MIN..MAX).random()
+    val i = MakeInterpreter("$x1 + $x2")
+    assertEquals(x1 + x2, i.Execute())
   }
 
   @Test
@@ -27,9 +46,25 @@ class InterpreterTest {
   }
 
   @Test
+  fun TestMinusRnd() {
+    val x1 = (MIN..MAX).random()
+    val x2 = (MIN..MAX).random()
+    val i = MakeInterpreter("$x1 - $x2")
+    assertEquals(x1 - x2, i.Execute())
+  }
+
+  @Test
   fun TestMul() {
     val i = MakeInterpreter("6 * 7")
     assertEquals(THE_ANSWER, i.Execute())
+  }
+
+  @Test
+  fun TestMulRnd() {
+    val x1 = (MIN..MAX).random()
+    val x2 = (MIN..MAX).random()
+    val i = MakeInterpreter("$x1 * $x2")
+    assertEquals(x1 * x2, i.Execute())
   }
 
   @Test
@@ -39,9 +74,27 @@ class InterpreterTest {
   }
 
   @Test
+  fun TestDivRnd() {
+    val x1 = (MIN..MAX).random()
+    val x2 = (MIN..MAX).random()
+    val i = MakeInterpreter("$x1 / $x2")
+    assertEquals(x1 / x2, i.Execute())
+  }
+
+  @Test
   fun TestPlusMinusPlusMinus() {
     val i = MakeInterpreter("12 + 10 - 6 + 56 - 30")
     assertEquals(THE_ANSWER, i.Execute())
+  }
+
+  @Test
+  fun TestPlusMinusRnd() {
+    val x1 = (MIN..MAX).random()
+    val x2 = (MIN..MAX).random()
+    val x3 = (MIN..MAX).random()
+    val x4 = (MIN..MAX).random()
+    val i = MakeInterpreter("$x1 + $x2 - $x3 + $x4")
+    assertEquals(x1 + x2 - x3 + x4, i.Execute())
   }
 
   @Test
@@ -51,10 +104,35 @@ class InterpreterTest {
   }
 
   @Test
+  fun TestComplexExpressionRnd0() {
+    val x1 = (MIN..MAX).random()
+    val x2 = (MIN..MAX).random()
+    val x3 = (MIN..MAX).random()
+    val x4 = (MIN..MAX).random()
+    val x5 = (MIN..MAX).random()
+    val x6 = (MIN..MAX).random()
+    val i = MakeInterpreter("$x1 / $x2 + $x3 * $x4 - $x5 * $x6")
+    assertEquals(x1 / x2 + x3 * x4 - x5 * x6, i.Execute())
+  }
+
+  @Test
   fun TestComplexExpression1() {
     val i = MakeInterpreter("(561 - 5 * (52 + 59)) * (28 - 21)")
     assertEquals(THE_ANSWER, i.Execute())
   }
+
+  @Test
+  fun TestComplexExpressionRnd1() {
+    val x1 = (MIN..MAX).random()
+    val x2 = (MIN..MAX).random()
+    val x3 = (MIN..MAX).random()
+    val x4 = (MIN..MAX).random()
+    val x5 = (MIN..MAX).random()
+    val x6 = (MIN..MAX).random()
+    val i = MakeInterpreter("(($x1 - $x2) * ($x3 + $x4)) * ($x5 - $x6)")
+    assertEquals(((x1 - x2) * (x3 + x4)) * (x5 - x6), i.Execute())
+  }
+
 }
 
 
